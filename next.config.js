3eafa6093df8967a774ab2a62818cea383257fda
kaustubh2408 <k.dwivedi@chai.finance>
2023-11-05 13:43:47 +0530

    Added new build

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
 
   env: {
     GOOGLE_CLIENT_ID:
-      "100521142658-epbpqe0is3pthk5m54s4o2ol6kapdt3j.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET: "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",
+      "382651915278-jk76g9e278vbe3dn6unnc43fmmbk7v36.apps.googleusercontent.com",
+    GOOGLE_CLIENT_SECRET: "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",
     GITHUB_CLIENT_ID: "76b7c0ddac9ddea2aea2",
     GITHUB_CLIENT_SECRET: "8a87fa6f999d867cb9d9f1372a768aaa8193f7f0",
   },
